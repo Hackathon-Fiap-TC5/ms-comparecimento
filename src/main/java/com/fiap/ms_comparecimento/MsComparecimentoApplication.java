471f390db7177e08860a89cb08d5f5f573d36a65
MVBPinho <marcos.vb.pinho@live.com>
2026-02-05 00:24:02 -0300

    Criação do projeto e swagger

---
+++ src/main/java/com/fiap/ms_comparecimento/MsComparecimentoApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.ms_comparecimento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsComparecimentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsComparecimentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/ms_comparecimento/MsComparecimentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.ms_comparecimento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsComparecimentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
